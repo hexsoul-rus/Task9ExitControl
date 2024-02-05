fefde0e2edb3c9e2de9aa3bb1005638fb95ce2b2
Asus <Asus@ASUS-PC31>
2024-02-05 13:27:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task9ExitControl
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string inputLine = "";
+            string exitCommand = "exit";
+
+            while (inputLine != exitCommand)
+            {
+                Console.WriteLine("У попа была собака, он ее любил.");
+                Console.WriteLine("Она съела кусок мяса, он ее убил!");
+                Console.WriteLine("В землю закопал, надпись написал, что:\n");
+
+                Console.Write("Для выхода введите " + exitCommand + " или нажмите Enter, чтобы продолжить: ");
+                inputLine = Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
